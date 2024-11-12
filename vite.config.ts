1423a3f135a8b514d4b3f9e63b1b92a1586ccd01
saber71 <1533357473@qq.com>
2024-11-13 07:16:55 +0800

    build(remote-addon-vue): 添加 Vue 插件依赖
    - 在 package.json 中添加 @vitejs/plugin-vue 和 @vitejs/plugin-vue-jsx 依赖
    - 这些插件将用于支持 Vue 开发和 JSX 语法

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,6 @@
 import { externalDepsPlugin } from "@heraclius/external-deps-plugin"
+import vuePlugin from "@vitejs/plugin-vue"
+import vueJsxPlugin from "@vitejs/plugin-vue-jsx"
 import swc from "unplugin-swc"
 import { defineConfig } from "vite"
 import { resolve } from "path"
@@ -7,7 +9,7 @@ import dtsPlugin from "vite-plugin-dts"
 export default defineConfig({
   root: resolve(__dirname, "test/page"),
   publicDir: resolve(__dirname, "test/page/public"),
-  plugins: [dtsPlugin({ rollupTypes: true }), swc.vite(), externalDepsPlugin()],
+  plugins: [dtsPlugin({ rollupTypes: true }), swc.vite(), externalDepsPlugin(), vueJsxPlugin(), vuePlugin()],
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.ts"),
